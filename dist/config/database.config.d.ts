b032bd013b924cfae6bdceb5a376a6b36f3004d9
philapulga10 <42233581+philapulga10@users.noreply.github.com>
2025-08-05 15:21:04 +0700

    Remove dist/ from tracking

--- dist/config/database.config.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class DatabaseConfig {
-    private configService;
-    constructor(configService: ConfigService);
-    getDatabaseUrl(): string;
-}
